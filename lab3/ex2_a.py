7ff8d69d0469e60e36c3509e466e3186ce9a1dcd
Buciu Theodor-Marian <theodor.buciu@proton.me>
2025-10-28 23:26:23 +0200

    pdf

--- lab3/ex2_a.py
+++ lab3/ex2_a.py
@@ -5,7 +5,7 @@ tau=np.pi*2
 circle=lambda x: np.e**(-1j*tau*x)
 
 
-f= lambda x: np.sin(tau*x*3)
+f= lambda x: np.sin(tau*x*5)
 ens=np.linspace(0,1,100)
 axs, fig=plt.subplots(1,2);
 fig[0].plot(ens,f(ens));
